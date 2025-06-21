a32f914351ad511670a58ea8c0b7cff496c2cd6c
Abdo-Karim <abdosalman@gmail.com>
2025-06-21 18:07:05 -0300

    Update perguntas.js

--- jogo/perguntas.js
+++ jogo/perguntas.js
@@ -1,8 +1,1 @@
-const questions = {
-    5: "Qual é o plural de lápis?",
-    10: "Quanto é 8 x 7?",
-    15: "Qual é a capital do Brasil?",
-    20: "O que é um triângulo equilátero?",
-    25: "Quanto é 100 dividido por 4?",
-    30: "Qual é o resultado de 12 + 18?"
-};
+
