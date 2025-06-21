768d69c9df2965763f0f8b3c32867b529fb2136d
Abdo-Karim <abdosalman@gmail.com>
2025-06-21 20:37:31 -0300

    Delete jogo/perguntas.js

--- jogo/perguntas.js
+++
@@ -1,8 +0,0 @@
-
-const perguntas = [
-    { pergunta: "Quanto é 5 + 3?", resposta: "8" },
-    { pergunta: "Qual é a capital do Brasil?", resposta: "Brasília" },
-    { pergunta: "Quanto é 9 - 4?", resposta: "5" },
-    { pergunta: "Quem descobriu o Brasil?", resposta: "Pedro Álvares Cabral" },
-    { pergunta: "Quanto é 6 x 2?", resposta: "12" }
-];
