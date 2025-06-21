ebe8a1e1574124b1373740f48b1bca27a19c1091
Abdo-Karim <abdosalman@gmail.com>
2025-06-21 20:39:51 -0300

    Add files via upload

---
+++ jogo/perguntas.js
@@ -0,0 +1,8 @@
+
+const perguntas = [
+    { pergunta: "Quanto é 5 + 3?", resposta: "8", opcoes: ["6", "8", "9"] },
+    { pergunta: "Qual é a capital do Brasil?", resposta: "Brasília", opcoes: ["Rio", "Brasília", "São Paulo"] },
+    { pergunta: "Quanto é 9 - 4?", resposta: "5", opcoes: ["4", "5", "6"] },
+    { pergunta: "Quem descobriu o Brasil?", resposta: "Pedro Álvares Cabral", opcoes: ["Dom Pedro", "Pedro Álvares Cabral", "Tiradentes"] },
+    { pergunta: "Quanto é 6 x 2?", resposta: "12", opcoes: ["12", "10", "14"] }
+];
